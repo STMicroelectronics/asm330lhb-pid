69ac50fbbbd8837b00f8b50a7e6f0279af00bd12
Gioele Fiorenza <gioele.fiorenza2000@gmail.com>
2026-02-06 15:45:22 +0100

    Fix odr_cal_reg_set/get API, change type uint8 to int8
    Signed-off-by: Gioele Fiorenza <gioele.fiorenza2000@gmail.com>

--- asm330lhb_reg.h
+++ asm330lhb_reg.h
@@ -895,7 +895,7 @@ typedef struct
 #define ASM330LHB_INTERNAL_FREQ_FINE           0x63U
 typedef struct
 {
-  uint8_t freq_fine                : 8;
+  int8_t freq_fine                 : 8;
 } asm330lhb_internal_freq_fine_t;
 
 #define ASM330LHB_X_OFS_USR                    0x73U
@@ -2021,8 +2021,8 @@ int32_t asm330lhb_acceleration_raw_get(const stmdev_ctx_t *ctx, int16_t *val);
 
 int32_t asm330lhb_fifo_out_raw_get(const stmdev_ctx_t *ctx, uint8_t *val);
 
-int32_t asm330lhb_odr_cal_reg_set(const stmdev_ctx_t *ctx, uint8_t val);
-int32_t asm330lhb_odr_cal_reg_get(const stmdev_ctx_t *ctx, uint8_t *val);
+int32_t asm330lhb_odr_cal_reg_set(const stmdev_ctx_t *ctx, int8_t val);
+int32_t asm330lhb_odr_cal_reg_get(const stmdev_ctx_t *ctx, int8_t *val);
 
 typedef enum
 {
