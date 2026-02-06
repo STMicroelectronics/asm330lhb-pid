3aefb86d59bd9d608b88edd79c442cd8f4169696
Gioele Fiorenza <gioele.fiorenza2000@gmail.com>
2026-02-06 15:55:24 +0100

    Change fsm_out_get return type
    Signed-off-by: Gioele Fiorenza <gioele.fiorenza2000@gmail.com>

--- asm330lhb_reg.h
+++ asm330lhb_reg.h
@@ -2510,7 +2510,6 @@ typedef struct
 } asm330lhb_fsm_status_t;
 int32_t asm330lhb_fsm_status_get(const stmdev_ctx_t *ctx,
                                  asm330lhb_fsm_status_t *val);
-int32_t asm330lhb_fsm_out_get(const stmdev_ctx_t *ctx, uint8_t *buff);
 
 int32_t asm330lhb_long_cnt_flag_data_ready_get(const stmdev_ctx_t *ctx,
                                                uint8_t *val);
@@ -2544,6 +2543,28 @@ int32_t asm330lhb_long_clr_set(const stmdev_ctx_t *ctx,
                                asm330lhb_fsm_lc_clr_t val);
 int32_t asm330lhb_long_clr_get(const stmdev_ctx_t *ctx,
                                asm330lhb_fsm_lc_clr_t *val);
+
+typedef struct
+{
+  asm330lhb_fsm_outs1_t    fsm_outs1;
+  asm330lhb_fsm_outs2_t    fsm_outs2;
+  asm330lhb_fsm_outs3_t    fsm_outs3;
+  asm330lhb_fsm_outs4_t    fsm_outs4;
+  asm330lhb_fsm_outs5_t    fsm_outs5;
+  asm330lhb_fsm_outs6_t    fsm_outs6;
+  asm330lhb_fsm_outs7_t    fsm_outs7;
+  asm330lhb_fsm_outs8_t    fsm_outs8;
+  asm330lhb_fsm_outs9_t    fsm_outs9;
+  asm330lhb_fsm_outs10_t    fsm_outs10;
+  asm330lhb_fsm_outs11_t    fsm_outs11;
+  asm330lhb_fsm_outs12_t    fsm_outs12;
+  asm330lhb_fsm_outs13_t    fsm_outs13;
+  asm330lhb_fsm_outs14_t    fsm_outs14;
+  asm330lhb_fsm_outs15_t    fsm_outs15;
+  asm330lhb_fsm_outs16_t    fsm_outs16;
+} asm330lhb_fsm_out_t;
+int32_t asm330lhb_fsm_out_get(const stmdev_ctx_t *ctx,
+                              asm330lhb_fsm_out_t *val);
 
 typedef enum
 {
