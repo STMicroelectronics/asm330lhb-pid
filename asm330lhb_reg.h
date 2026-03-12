b78f946f2673888b04a40de26acdd3c031614faf
Gioele Fiorenza <gioele.fiorenza2000@gmail.com>
2026-03-12 14:57:00 +0100

    Fix hp_slope_xl_en_t LP_ODR_DIV_4 hex value
    Signed-off-by: Gioele Fiorenza <gioele.fiorenza2000@gmail.com>

--- asm330lhb_reg.h
+++ asm330lhb_reg.h
@@ -1652,7 +1652,7 @@ typedef enum
   ASM330LHB_HP_ODR_DIV_400            = 0x16,
   ASM330LHB_HP_ODR_DIV_800            = 0x17,
   ASM330LHB_LP_ODR_DIV_2              = 0x01,
-  ASM330LHB_LP_ODR_DIV_4              = 0x00,
+  ASM330LHB_LP_ODR_DIV_4              = 0x80,
   ASM330LHB_LP_ODR_DIV_10             = 0x81,
   ASM330LHB_LP_ODR_DIV_20             = 0x82,
   ASM330LHB_LP_ODR_DIV_45             = 0x83,
